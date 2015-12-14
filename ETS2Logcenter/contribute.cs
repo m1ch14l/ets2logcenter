0e0103237a9b2021327b735bf33946fe2342ea85
m1ch14l <hellinxmichiel@hotmail.com>
2015-12-14 15:43:22 +0100

    v1
    v1

---
+++ ETS2Logcenter/contribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ETS2Logcenter
+{
+    public partial class contribute : Form
+    {
+        public contribute()
+        {
+            InitializeComponent();
+        }
+
+        private void contribute_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
